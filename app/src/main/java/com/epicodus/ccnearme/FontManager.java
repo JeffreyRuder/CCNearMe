6ecc3a1b740a40b049b320a7f9fa5f0516a84592
Jeffrey Ruder <ruderjt@gmail.com>
2016-03-18 10:08:58 -0700

    Setup FontManager and implement FontAwesome icons

---
+++ app/src/main/java/com/epicodus/ccnearme/FontManager.java
@@ -0,0 +1,17 @@
+package com.epicodus.ccnearme;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by Jeffrey on 3/18/2016.
+ */
+public class FontManager {
+
+    public static final String ROOT = "fonts/",
+            FONTAWESOME = ROOT + "fontawesome-webfont.ttf";
+
+    public static Typeface getTypeface(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+}
---
+++ app/src/main/java/com/epicodus/ccnearme/FontawesomeIconTextView.java
@@ -0,0 +1,16 @@
+package com.epicodus.ccnearme;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Jeffrey on 3/18/2016.
+ */
+public class FontAwesomeIconTextView extends TextView {
+    public FontAwesomeIconTextView (Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setTypeface(FontManager.getTypeface(context, FontManager.FONTAWESOME));
+    }
+}
