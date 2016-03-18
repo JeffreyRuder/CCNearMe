0c39e62517ea473e37b7bae4a62715d324fa2f9d
Jeffrey Ruder <ruderjt@gmail.com>
2016-03-18 12:12:05 -0700

    Add AboutActivity

--- app/src/main/java/com/epicodus/ccnearme/MainActivity.java
+++ app/src/main/java/com/epicodus/ccnearme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.ccnearme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -90,8 +91,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_about) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
